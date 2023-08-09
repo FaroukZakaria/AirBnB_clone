94b247dea4685a42a3713ea069ef6c1e1e54d334
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-08-09 16:34:57 +0000

    task answer

--- models/base_model.py
+++ models/base_model.py
@@ -1,18 +1,28 @@
 #!/usr/bin/python3
 import uuid
 from datetime import datetime
+"""Creating a super class which subclasses would inherit from"""
 
 
 class BaseModel():
+    """The initialization of Model attributes"""
     def __init__(self):
+        """Definitions"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+
     def __str__(self):
+        """String representation"""
         return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
+
     def save(self):
+        """Update modification date"""
         self.updated_at = datetime.now()
+
     def to_dict(self):
+        """Returns a dictionary representation of Class
+            with keyworded items and includes class name"""
         for key, value in self.__dict__.items():
             if key == 'created_at' or key == 'updated_at':
                 self.__dict__[key] = value.isoformat()
