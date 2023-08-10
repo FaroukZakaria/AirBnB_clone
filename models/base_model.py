eecf2e6a87ebd38f5c6bd8cd4ad1f5ab1ab91376
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-08-10 13:53:33 +0000

    task answer

--- models/base_model.py
+++ models/base_model.py
@@ -6,11 +6,23 @@ from datetime import datetime
 
 class BaseModel():
     """The initialization of Model attributes"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Definitions"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, value)
+
+            if 'created_at' in kwargs:
+                self.created_at = kwargs['created_at'].isoformat()
+
+            if 'updated_at' in kwargs:
+                self.updated_at = kwargs['updated_at'].isoformat()
+
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """String representation"""
