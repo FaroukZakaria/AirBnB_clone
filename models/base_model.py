062522bad546e4350194d60975e45b4ed7744f87
FaroukZakaria <GamesWithoutLose97@gmail.com>
2023-08-10 14:03:11 +0000

    task answer

--- models/base_model.py
+++ models/base_model.py
@@ -14,10 +14,10 @@ class BaseModel():
                     setattr(self, key, value)
 
             if 'created_at' in kwargs:
-                self.created_at = kwargs['created_at'].isoformat()
+                self.created_at = datetime.strptime(kwargs['created_at'], "%Y-%m-%dT%H:%M:%S.%f")
 
             if 'updated_at' in kwargs:
-                self.updated_at = kwargs['updated_at'].isoformat()
+                self.updated_at = datetime.strptime(kwargs['updated_at'], "%Y-%m-%dT%H:%M:%S.%f")
 
         else:
             self.id = str(uuid.uuid4())
